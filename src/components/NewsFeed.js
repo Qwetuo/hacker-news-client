f0b3ca3ae0c77429e78aa0894d23aa05e516e0c0
qwetuo <chewminzhuang@hotmail.com>
2018-06-18 16:33:34 +0800

    commit

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
+import {articles} from "../seedData";
+// import UpVote from "./UpVote";
 
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: articles,
+      votes: articles.votes
     };
   }
 
   render() {
     return this.state.articles.map((article, i) => {
       // TODO: start coding!
-      return <li key={i}></li>;
+      return <li key={i}>{article.title} <a href={article.url}  className="details">(link)</a>
+      <p className="description">{article.description}</p>
+      <p className="details">
+      <button onClick={(event) => this.handleUpVoteBtn(i, event)}>up vote{article.votes}</button>
+      {/* <UpVote votes={articles.votes} handleUpVoteBtn={this.handleUpVoteBtn}  />  */}
+      {this.state.articles[i].votes} points by {article.author != null ? article.author : "unknown"} | Published on {article.publishedAt}</p>
+      </li>;
     });
+  }
+  handleUpVoteBtn = (i, event) => {
+    console.log(this.state.articles[i].votes)
+    // const updatedArticle = {
+    //   this.state.articles[i].votes:
+    // }
+    // this.setState({
+      
+    // })
   }
 }
 
---
+++ src/components/UpVote.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+
+// const UpVote = (props) => {
+//     return <p><button onClick={event => props.handleUpVoteBtn(event)}>up vote</button>{props.votes}</p>
+
+// }
+
+// export default UpVote;
