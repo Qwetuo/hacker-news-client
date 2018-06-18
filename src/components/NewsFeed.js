8b7235b3511a6c2b705401899d64b55d5d3a2160
qwetuo <chewminzhuang@hotmail.com>
2018-06-18 22:15:57 +0800

    completed assignment

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,36 +1,48 @@
 import React, { Component } from "react";
-import {articles} from "../seedData";
-// import UpVote from "./UpVote";
+import { articles } from "../seedData";
 
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: articles,
-      votes: articles.votes
+      articles: articles
     };
   }
 
   render() {
     return this.state.articles.map((article, i) => {
-      // TODO: start coding!
-      return <li key={i}>{article.title} <a href={article.url}  className="details">(link)</a>
-      <p className="description">{article.description}</p>
-      <p className="details">
-      <button onClick={(event) => this.handleUpVoteBtn(i, event)}>up vote{article.votes}</button>
-      {/* <UpVote votes={articles.votes} handleUpVoteBtn={this.handleUpVoteBtn}  />  */}
-      {this.state.articles[i].votes} points by {article.author != null ? article.author : "unknown"} | Published on {article.publishedAt}</p>
-      </li>;
+      return (
+        <li key={i}>
+          {article.title}{" "}
+          <a href={article.url} className="details">
+            (link)
+          </a>
+          <p className="description">{article.description}</p>
+          <p className="details">
+            <button onClick={() => this.handleUpVoteBtn(article, i)}>
+              up vote
+            </button>
+            | {article.votes} points by{" "}
+            {article.author != null ? article.author : "unknown"} | Published on{" "}
+            {article.publishedAt}
+          </p>
+        </li>
+      );
     });
   }
-  handleUpVoteBtn = (i, event) => {
-    console.log(this.state.articles[i].votes)
-    // const updatedArticle = {
-    //   this.state.articles[i].votes:
-    // }
-    // this.setState({
-      
-    // })
+  handleUpVoteBtn(article, i) {
+    const count = article.votes + 1;
+    const updatedArticle = {
+      ...article,
+      votes: count
+    };
+    this.setState({
+      articles: [
+        ...this.state.articles.slice(0, i),
+        updatedArticle,
+        ...this.state.articles.slice(i + 1)
+      ]
+    });
   }
 }
 
--- src/components/UpVote.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-
-// const UpVote = (props) => {
-//     return <p><button onClick={event => props.handleUpVoteBtn(event)}>up vote</button>{props.votes}</p>
-
-// }
-
-// export default UpVote;
